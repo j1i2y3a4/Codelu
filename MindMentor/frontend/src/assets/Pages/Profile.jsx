2f3d9f0b0ad2fa57a5b13ec785450d09b8c69a26
meenakshysudhir <ukms2004@gmail.com>
2024-06-02 19:33:50 +0530

    koo

--- MindMentor/frontend/src/assets/Pages/Profile.jsx
+++ MindMentor/frontend/src/assets/Pages/Profile.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Navbar from "../Components/Navbar";
 const Profile = () => {
@@ -8,23 +7,6 @@ const Profile = () => {
       <h1>Profile page</h1>
     </div>
   );
-=======
-import React from 'react';
-import Navbar from '../Components/Navbar';
-import '../Components/CSS/Profile.css';
-
-const Profile = () => {
-    return (
-        <div className='profile'>
-            {/* <nav>
-                <Navbar/>
-            </nav> */}
-            <div className="profile-card">
-                <h2>PROFILE</h2>
-            </div>
-        </div>
-
-    );
 };
 
 export default Profile;
