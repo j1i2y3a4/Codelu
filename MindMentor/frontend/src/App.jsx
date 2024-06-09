72a5baeb57ebc3c73215cfe44bb56d03532ac2db
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-06-09 12:29:35 +0530

    frontend not updated

--- MindMentor/frontend/src/App.jsx
+++ MindMentor/frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import AppRoutes from './Routes';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./assets/Components/Navbar";
+import Profile from "./assets/Pages/Profile";
+import Chat from "./assets/Pages/Chat";
+import Task from "./assets/Pages/Task";
+import Login from "./assets/Pages/Login";
+import "./App.css"
 
 const App = () => {
     return (
-        <div className="App">
-            <AppRoutes />
-        </div>
+        <Router className="app">
+            <Routes>
+                <Route path="/" element={<Login/>} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/tasks" element={<Task />}/>
+            </Routes>
+        </Router>
     );
 };
 
