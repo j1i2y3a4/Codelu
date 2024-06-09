222a61e4f467d103d5d44d4d97447ae9507b5919
jiyarosejoshy <jiyarosejoshy2004@gmail.com>
2024-06-09 13:35:04 +0530

    profile

--- MindMentor/frontend/src/assets/Pages/Profile.jsx
+++ MindMentor/frontend/src/assets/Pages/Profile.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import "../Components/CSS/Profile.css";
 const Profile = () => {
   return (
     <div className="profile">
-      <Navbar />
-      <h1>Profile page</h1>
+        <div className="nav">
+            <Navbar />
+        </div>
+        <div className="user-bio">
+            <h2>WELCOME, JIYA ROSE JOSHY</h2>
+        </div>
     </div>
   );
 };
